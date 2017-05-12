0ca5b641500638515e9c06d3910be0846c586899
Jamie Wang <sephona@gmail.com>
2017-05-12 12:08:40 -0400

    First walk through mod functions

--- app.js
+++ app.js
@@ -16,16 +16,36 @@ function addItem(state, item) {
 
 //check item off
 function checkItem(state, item) {
-  if(state.items.title === item) {
-      return state.items.checked = false;
-  } else {
-      return state.items.check = true;
+  for(let i = 0; i < state.items.length; i++) {
+    if(state.items[i].title === item && state.items.i.checked) {
+      return state.items[i].checked = false;
+    } else {
+      return state.items[i].checked = true;
+    }
   }
 }
 
-console.log(checkItem(stateObj, 'pineapples'));
-
 //delete item
+function deleteItem(state, item) {
+  //loop through items array
+  //find the object where title value is the item we are looking for
+  //delete that object from the array
+    //use splice? 
+        //If so, need index of where to start splicing
+        //indexOf that object item 
+
+  for(let i = 0; i < state.items.length; i++) {
+      if(state.items[i].title === item) {
+          return state.items.splice(state.items.indexOf(state.items[i].title === item), 1);
+      } 
+  }
+
+ arr = state.items 
+ arr = ['cat', 'dog', 'hamster'];
+ //arr.indexOf('cat') => 0
+ //state.items.indexOf
+ //splice(1,1)
+}
 
 
 // 3. State rendering functions. Write some code that renders your state into a DOM element. The function will take in the state object and the element you want to update, then it will format your user input, and push it onto the dom element.
